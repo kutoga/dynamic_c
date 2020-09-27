dc90e202aa3ac50c4e718a0de0b5b6eb63f0b145
Benjamin B. Meier <benjamin.meier70@gmail.com>
2020-09-27 09:55:42 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,6 @@
+# DynamicC
+
+DynamicC is a simple and stupic library with just two API calls: It can be used
+to compile code at runtime. This might heavily improve the performance of some
+code (compared to a generic solution which).
+
--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 #include "dynamicc.h"
 
+int my_int = 12;
+
 int main()
 {
     dynamic_c_t cc = dc_init();
     fprintf(cc.f, "#include <stdio.h>\n");
     fprintf(cc.f, "int f() {");
     fprintf(cc.f, "    printf(\"hello world!\\n\");");
+    fprintf(cc.f, "    ++*(int *)%ld;", (long)&my_int);
     fprintf(cc.f, "    return 0;");
     fprintf(cc.f, "}");
     void *so_handle = dc_finish(&cc);
 
     int (*f)() =  dlsym(so_handle, "f");
     printf("%p\n", f);
+    printf("my_int = %d\n", my_int);
     f();
+    printf("my_int = %d\n", my_int);
 
     dlclose(so_handle);
 }
